fb0b02985906f762d1c19763f3d1d0f631c3be66
Gaston Severina <gaston.e.severina@intel.com>
2014-09-24 23:20:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include 'lib/bones.php';
+
+get('/', function($app) {
+    $app->set('message', 'Welcome Back!');
+    $app->render('home');
+});
+
+get(('/signup'), function ($app) {
+    $app->render('signup');
+});
+
+post('/signup', function($app) {
+    $app->set('message', 'Thanks for Signing Up ' . $app->form('name') . '!');
+    $app->render('home');
+});
